e082c7e5cf63b3d6f1bd93616c413f3cf079103c
Dasha <Dasha@DESKTOP-I5KMMF9>
2024-11-08 22:07:49 +0300

    Добавьте файлы проекта.

---
+++ Form2.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace t
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.numGrade = new System.Windows.Forms.NumericUpDown();
+            this.dateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.listBoxGrades = new System.Windows.Forms.ListBox();
+            this.btnAddGrade = new System.Windows.Forms.Button();
+            this.btnAnalyze = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numGrade)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(38, 372);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 22);
+            this.txtName.TabIndex = 0;
+            this.txtName.TextChanged += new System.EventHandler(this.txtName_TextChanged);
+            // 
+            // numGrade
+            // 
+            this.numGrade.Location = new System.Drawing.Point(170, 373);
+            this.numGrade.Maximum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.numGrade.Name = "numGrade";
+            this.numGrade.Size = new System.Drawing.Size(98, 22);
+            this.numGrade.TabIndex = 1;
+            this.numGrade.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // dateTimePicker
+            // 
+            this.dateTimePicker.Location = new System.Drawing.Point(300, 373);
+            this.dateTimePicker.Name = "dateTimePicker";
+            this.dateTimePicker.Size = new System.Drawing.Size(168, 22);
+            this.dateTimePicker.TabIndex = 2;
+            // 
+            // listBoxGrades
+            // 
+            this.listBoxGrades.FormattingEnabled = true;
+            this.listBoxGrades.ItemHeight = 16;
+            this.listBoxGrades.Location = new System.Drawing.Point(38, 58);
+            this.listBoxGrades.Name = "listBoxGrades";
+            this.listBoxGrades.ScrollAlwaysVisible = true;
+            this.listBoxGrades.Size = new System.Drawing.Size(430, 260);
+            this.listBoxGrades.TabIndex = 3;
+            // 
+            // btnAddGrade
+            // 
+            this.btnAddGrade.Location = new System.Drawing.Point(487, 371);
+            this.btnAddGrade.Name = "btnAddGrade";
+            this.btnAddGrade.Size = new System.Drawing.Size(99, 23);
+            this.btnAddGrade.TabIndex = 4;
+            this.btnAddGrade.Text = "Добавить";
+            this.btnAddGrade.UseVisualStyleBackColor = true;
+            this.btnAddGrade.Click += new System.EventHandler(this.btnAddGrade_Click_1);
+            // 
+            // btnAnalyze
+            // 
+            this.btnAnalyze.Location = new System.Drawing.Point(487, 67);
+            this.btnAnalyze.Name = "btnAnalyze";
+            this.btnAnalyze.Size = new System.Drawing.Size(99, 23);
+            this.btnAnalyze.TabIndex = 5;
+            this.btnAnalyze.Text = "Рейтинг";
+            this.btnAnalyze.UseVisualStyleBackColor = true;
+            this.btnAnalyze.Click += new System.EventHandler(this.btnAnalyze_Click_1);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(487, 415);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(99, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Удалить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(608, 465);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnAnalyze);
+            this.Controls.Add(this.btnAddGrade);
+            this.Controls.Add(this.listBoxGrades);
+            this.Controls.Add(this.dateTimePicker);
+            this.Controls.Add(this.numGrade);
+            this.Controls.Add(this.txtName);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            ((System.ComponentModel.ISupportInitialize)(this.numGrade)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.NumericUpDown numGrade;
+        private System.Windows.Forms.DateTimePicker dateTimePicker;
+        private System.Windows.Forms.ListBox listBoxGrades;
+        private System.Windows.Forms.Button btnAddGrade;
+        private System.Windows.Forms.Button btnAnalyze;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ Form2.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace t
+{
+    public partial class Form2 : Form
+    {
+        // Список для хранения данных о студентах и их оценках
+        //private List<string> studentGrades = new List<string>();
+        private List<int> grades = new List<int>();
+        private List<string> students = new List<string>();
+        private List<DateTime> dates = new List<DateTime>();
+
+        public Form2()
+        {
+            InitializeComponent();
+        }
+        // Обработчик для кнопки "Добавить оценку"
+        private void btnAddGrade_Click_1(object sender, EventArgs e)
+        {
+            string studentName = txtName.Text;  //извлекает фамилию студента из текстового поля
+            int grade = (int)numGrade.Value;    
+            DateTime gradeDate = dateTimePicker.Value;
+
+            if (string.IsNullOrEmpty(studentName))
+            {
+                MessageBox.Show("Введите имя студента.");
+                return;
+            }
+
+            // Добавляем данные в списки
+            students.Add(studentName);
+            grades.Add(grade);
+            dates.Add(gradeDate);
+
+            // Очистка полей ввода
+            txtName.Clear();
+            numGrade.Value = 0;
+            dateTimePicker.Value = DateTime.Now;
+
+            // Обновление списка оценок на форме
+            UpdateGradeList();
+        }
+
+        // Обновление списка оценок на форме
+        private void UpdateGradeList()
+        {
+            listBoxGrades.Items.Clear();//очищаем список, чтобы добавить обновлённые данные
+            for (int i = 0; i < students.Count; i++)//перебирает все элементы в списке студентов 
+            {
+                listBoxGrades.Items.Add($"{students[i]} - {grades[i]} - {dates[i].ToShortDateString()}");
+            }//dates[i].ToShortDateString() — это преобразует дату в строку вида "dd.MM.yyyy"
+        }
+
+        // Обработчик для кнопки "Анализировать"
+        private void btnAnalyze_Click_1(object sender, EventArgs e)
+        {
+            // Проверяем, есть ли оценки
+            if (grades.Count == 0)
+            {
+                MessageBox.Show("Нет оценок для анализа.");
+                return;
+            }
+
+            //создание словаря, который будет хранить для каждого студента его список оценок
+            Dictionary<string, List<int>> studentGradesDict = new Dictionary<string, List<int>>();
+
+            // Заполняем словарь оценками студентов
+            for (int i = 0; i < students.Count; i++)
+            {
+                string student = students[i];
+                int grade = grades[i];
+
+                ////проверяем, есть ли уже в словаре запись для этого студента
+                if (!studentGradesDict.ContainsKey(student)) 
+                {//если нет, создаем новую запись для этого студента с пустым списком оценок
+                    studentGradesDict[student] = new List<int>();
+                }
+                //добавляем оценку в список оценок этого студента
+                studentGradesDict[student].Add(grade);
+                
+            }
+
+            // Переменные для хранения лучшего студента
+            string bestStudent = "";
+            double bestAverageGrade = -1;  // Начальное значение для лучшей средней оценки
+
+            // Формируем строку для вывода средней оценки каждого студента
+            string resultMessage = "Средние оценки студентов:\n";
+
+            foreach (var student in studentGradesDict)//начинаем проходить по всем записям в словаре
+            {
+                string studentName = student.Key;
+                List<int> gradesForStudent = student.Value;//список оценок
+
+                // Вычисляем среднюю оценку
+                double averageGrade = gradesForStudent.Average();
+
+                // Добавляем среднюю оценку в строку
+               // resultMessage += $"{studentName}: {averageGrade:F2}\n";
+
+                // Проверяем, если эта средняя оценка лучше, чем у текущего лучшего студента
+                if (averageGrade > bestAverageGrade)
+                {
+                    bestAverageGrade = averageGrade;
+                    bestStudent = studentName;
+                }
+            }
+
+            // Добавляем информацию о лучшем студенте
+            resultMessage += $"\nЛучший ученик: {bestStudent} с средней оценкой: {bestAverageGrade:F2}";
+
+            // Показываем сообщение
+            MessageBox.Show(resultMessage);
+        }
+
+        private void txtName_TextChanged(object sender, EventArgs e)
+        {
+            if (txtName.Text.Length > 0)
+            {
+                // Проверка первой буквы на заглавную
+                if (!char.IsUpper(txtName.Text[0]))
+                {
+                    // Если первая буква не заглавная, то исправляем ее
+                    txtName.Text = char.ToUpper(txtName.Text[0]) + txtName.Text.Substring(1);
+                    // Устанавливаем курсор в конец текста
+                    txtName.SelectionStart = txtName.Text.Length;
+                }
+
+                // Проверка на наличие цифр и знаков
+                foreach (char c in txtName.Text)
+                {
+                    if (!char.IsLetter(c))
+                    {
+                        // Если найдена цифра или знак, то удаляем ее
+                        txtName.Text = txtName.Text.Replace(c.ToString(), "");
+                        // Устанавливаем курсор в конец текста
+                        txtName.SelectionStart = txtName.Text.Length;
+                        break;
+                    }
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (listBoxGrades.SelectedIndex != -1)
+            {
+                int index = listBoxGrades.SelectedIndex;
+
+                // Удаляем данные из списков
+                students.RemoveAt(index);
+                grades.RemoveAt(index);
+                dates.RemoveAt(index);
+
+                // Обновляем список
+                UpdateGradeList();
+            }
+            else
+            {
+                MessageBox.Show("Выберите запись для удаления.");
+            }
+        }
+    }
+}
